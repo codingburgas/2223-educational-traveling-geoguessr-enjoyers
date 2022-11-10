a598960815408010b663457c30959d3d0cb32f3f
MIZlatev20 <mizlatev20@codingburgas.bg>
2022-11-10 12:06:33 +0200

    Add Menu

--- Application/geoguessr enjoyers/geoguessr enjoyers.cpp
+++ Application/geoguessr enjoyers/geoguessr enjoyers.cpp
@@ -12,7 +12,70 @@
 
 using namespace std;
 
+enum
+{
+	BLACK = 0,
+	RED = FOREGROUND_INTENSITY | FOREGROUND_RED,
+	YELLOW = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN,
+	WHITE = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
+};
+
+WORD displayColor[] = { WHITE, WHITE };
+
+const int xStartPosition = 50;
+
+void mainMenu()
+{
+	Logo();
+
+	int counter = 1;
+	char key;
+
+	while (true)
+	{
+		if (counter >= 1 && counter <= 4)
+			displayColor[counter - 1] = RED;
+
+		gotoxy(52, 11);
+		colorSelection(displayColor[0]);
+		cout << "1. Start";
+
+		gotoxy(52, 12);
+		colorSelection(displayColor[1]);
+		cout << "2. Exit";
+
+		key = _getch();
+
+		if (key == 80 && (counter >= 1 && counter <= 1)) // 80 - down arrow (keyboard)
+			counter++;
+		if (key == 72 && (counter >= 2 && counter <= 3)) // 72 - up arrow (keyboard)
+			counter--;
+
+		//carriage return - enter (keyboard)
+		if (key == '\r')
+		{
+			for (int i = 0; i < 2; i++)
+				displayColor[i] = WHITE;
+
+			system("CLS");
+
+			if (counter == 1)
+			{
+
+				break;
+			}
+
+			else if (counter == 2)
+			{
+				exit(EXIT_SUCCESS);
+			}
+		}
+		for (int i = 0; i < 2; i++)
+			displayColor[i] = WHITE;
+	}
+}
+
 int main()
 {
-
+	mainMenu();
 }
