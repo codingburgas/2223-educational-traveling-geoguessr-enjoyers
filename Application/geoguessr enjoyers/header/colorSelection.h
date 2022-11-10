98ce96da326a2d2cf2a88796faec33c82578f1f2
MIZlatev20 <mizlatev20@codingburgas.bg>
2022-11-10 11:59:38 +0200

    Add colorSelection header file

---
+++ Application/geoguessr enjoyers/header/colorSelection.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+bool colorSelection(WORD newColor)
+{
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	if (hStdOut != INVALID_HANDLE_VALUE)
+	{
+		return SetConsoleTextAttribute(hStdOut, newColor);
+	}
+
+	return false;
+}
