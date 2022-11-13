7fa0dfd6de87f58d2e131027be954198cef611b7
MIZlatev20 <mizlatev20@codingburgas.bg>
2022-11-13 21:44:10 +0200

    Add ASCII Art

--- Application/geoguessr enjoyers/geoguessr enjoyers.cpp
+++ Application/geoguessr enjoyers/geoguessr enjoyers.cpp
@@ -8,7 +8,10 @@
 
 #include "header/colorSelection.h"
 #include "header/gotoxy.h"
-#include "header/Logo.h"
+#include "header/logo.h"
+#include "header/germany.h"
+#include "header/eiffelTower.h"
+#include "header/bulgaria.h"
 
 using namespace std;
 
@@ -75,73 +78,107 @@ void mainMenu()
 	}
 }
 
+void game()
+{
+	string name;
+	int choice;
+	int money;
+	
+	cout << "Hello, player! Please enter your name below!" << endl << endl;
+	
+	cin >> name;
+	cout << endl;
+
+	cout << "Nice to meet you, " << name << "!" << endl;
+	cout << "Hope you have fun while playing this!" << endl << endl;
+	cout << "Press enter to continue";
+
+	Sleep(1000);
+
+	 int key = _getch();
+	 system("cls");
+
+	cout << "After saving up enough money, you decide to explore different countries in Europe." << endl;
+	Sleep(1000);
+	cout << "You will visit some of the most popular monuments in the country and you will be asked a question for the following monument." << endl;
+	Sleep(1000);
+	cout << "Answer it right and you will get points. If you manage to answer the second question (it is going to be a difficult one), you will get bonus points." << endl;
+	Sleep(1000);
+	cout << "Try to achieve the most points and win." << endl << endl;
+	cout << "Press enter to continue";
+	Sleep(1000);
+	
+	key = _getch();
+	system("cls");
+
+	cout << "Your starting point is Bulgaria." << endl << endl;
+	
+	cout << "Which country will you visit? (Germany - 1; France - 2; Poland - 3; Spain - 4; Italy - 5; Czech Republic - 6; Greece - 7; Romania - 8)" << endl;
+	cout << "Enter the number of the country: ";
+
+	cin >> choice;
+	system("cls");
+
+	if (choice == 1)
+	{
+		cout << "Welcome to Germany!";
+		germany();
+
+		cout << "You arrive in Berlin, eager to begin your trip around Europe. Here's the first question!" << endl << endl;
+		cout << "What currency is used in Germany?" << endl;
+
+		cout << R"(
+			  _______________
+           _(               ),     ______________   ___________
+          (_   A: Euro       _)    _\  B: Lev    _\ (_          )_
+            (_______________)_    \_____________\    (_ C: Krone  )_
+                            (_)             (_         (___________)
+		)" << '\n';
+
+		key = _getch();
+
+		if (key == 'A' || key == 'a')
+		{
+			cout << "Correct!";
+		}
+	}
+	else if (choice == 2)
+	{
+		cout << "Welcome to France!";
+		eiffelTower();
+	}
+	else if (choice == 3)
+	{
+		cout << "Welcome to Poland!";
+	}
+	else if (choice == 4)
+	{
+		cout << "Welcome to Spain!";
+	}
+	else if (choice == 5)
+	{
+		cout << "Welcome to Italy!";
+	}
+	else if (choice == 6)
+	{
+		cout << "Welcome to Czech Republic!";
+	}
+	else if (choice == 7)
+	{
+		cout << "Welcome to Greece!";
+	}
+	else if(choice == 8)
+	{
+		cout << "Welcome to Romania!";
+	}
+
+}
+
 int main()
 {
 	mainMenu();
 
-	string name;
-    int choice;
-    int money;
+	game();
 
-    cout << "Hello, player! Please enter your name below!" << endl;
-
-    cin >> name;
-
-    cout << "Nice to meet you, " << name << "!" << endl;
-    cout << "Hope you have fun while playing this!" << endl << endl;
-    cout << "Press enter to continue";
-    Sleep(1000);
-    system("cls");
-
-    cout << "After saving up enough money, you decide to explore different countries in Europe." << endl;
-    Sleep(1000);
-    cout << "You will visit some of the most popular monuments in the country and you will be asked a question for the following monument." << endl;
-    Sleep(1000);
-    cout << "Answer it right and you will get points. If you manage to answer the second question (it is going to be a difficult one), you will get bonus points." << endl;
-    Sleep(1000);
-    cout << "Try to achieve the most points and win." << endl << endl;
-    cout << "Press enter to continue";
-    Sleep(1000);
-    system("cls");
-
-    cout << "Your starting point is Bulgaria." << endl;
-    cout << "What is the country that you will visit? (Germany - 1; France - 2; Poland - 3; Spain - 4; Italy - 5; Czech Republic - 6; Greece - 7; Romania - 8)" << endl;
-    cout << "Enter the number of the country: ";
-    cin >> choice;
-
-	switch (choice)
-	{
-		case 1:
-		{
-			cout << "Welcome to Germany!";
-		}
-		case 2:
-		{
-			cout << "Welcome to France!";
-		}
-		case 3:
-		{
-			cout << "Welcome to Poland!";
-		}
-		case 4:
-		{
-			cout << "Welcome to Spain!";
-		}
-		case 5:
-		{
-			cout << "Welcome to Italy!";
-		}
-		case 6:
-		{
-			cout << "Welcome to Czech Republic!";
-		}
-		case 7:
-		{
-			cout << "Welcome to Greece!";
-		}
-		case 8:
-		{
-			cout << "Welcome to Romania!";
-		}
-	}
+	
 }
---
+++ Application/geoguessr enjoyers/header/germany.h
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void germany()
+{
+	cout << R"(
+		                 \..,.-
+                 .\   |         __
+                 .|    .-     /  .;
+           _      _|      \__/..     \
+          \ ...\|   X Hamburg        :_
+           |                           \
+          /                            /
+         -.                           |
+          \  X Rheine      Berlin X    \
+       __/                              |
+      |                                 /
+      |                                 \
+     /                                   |
+     \     X Cologne        Dresden  X . ,
+      \                            ._-. .
+     /                        __.-/
+     |         X Frankfurt    \
+      \                        \
+       \                        \
+        ...,.                    \
+            /                     \.
+           /                       ,.
+          /                      ./
+         |         Munich X     |
+         \,......,__  __,  __.-. .
+                    \/   -/     ..
+                                                                
+)" << '\n';
+}
